a17ac7c01cfd9265bc35aa5fecfb1bff5053b74c
Why2312 <gabi1casap@gmail.com>
2023-12-10 11:46:13 +0200

    real (i updated turret code, hopefully it works)

--- code/turret/j.lua
+++ code/turret/j.lua
@@ -4,33 +4,82 @@ lifesensor = port1:GetPart("LifeSensor")
 gyro = port1:GetPart("Gyro")
 instrument = port1:GetPart("Instrument")
 switch = port1:GetPart("Switch")
-raycaster = port1:GetPart("Raycaster")
+raycasters = port1:GetParts("Raycaster")
+speaker = port1:GetPart("Speaker")
+microphone = port1:GetPart("Microphone")
+ssd = Machine:GetExpansions().SSD
 print(lifesensor)
-playerwhitelist = {"GenericRblxStudioDev", "ihaterover2001", "ognistylisel", "Subaru112", "Nadgob1", "mantux_he6yearsold", "GazaAudio", "xBarkDog500"}
-
-
+playerwhitelist = JSONDecode(ssd:Read("whitelist")) or {}
+local prefix = "!t "
+local aimingCoroutine = nil
 WEIGHTS = {
     health = 2.1, -- Multiplication
     distance = 1.3, -- Multiplication
     previous = 2.0, -- Addition
 }
 
+
+local function EvaluateRaycasterTarget(result)
+    if result and result.Type == "Humanoid" then
+        if isintable(playerwhitelist, result.Name) or isintable(playerwhitelist, result.LockedBy) then
+            return false -- Do not fire at whitelisted players
+        else
+            return true -- Target is valid for firing
+        end
+    end
+    return false -- Default to not firing
+end
+local function FindClosestTarget(reading, currentpos, previous)
+    local closest = {nil, math.huge, 0}
+    for _, v in pairs(reading) do
+        if not isintable(playerwhitelist, v.Name) and v.Health > 0 then
+            local pos = v.Position
+            local mag = (pos - currentpos).Magnitude
+            local weight = (v.Health / 100 * WEIGHTS.health) + (1 - mag / far * WEIGHTS.distance)
+            if v.ID == previous then weight += WEIGHTS.previous end
+            if weight > closest[3] then
+                closest = {v, mag, weight}
+            end
+        end
+    end
+    return closest[1]  -- Return the closest target object or nil
+end
 local near = 5 -- studs
 local far = 200 -- studs
-function CalculateRotation(currentpos, targetpos)
-    -- Calculate the direction vector from currentpos to targetpos
+local function CalculateRotation(currentpos, targetpos, currentRoll)
     local direction = (targetpos - currentpos).unit * -1
-
-    -- Calculate yaw (rotation around the y-axis)
     local yaw = math.deg(math.atan2(direction.x, direction.z))
-
-    -- Calculate pitch (rotation around the x-axis) using atan2
     local pitch = math.deg(math.atan2(direction.y, math.sqrt(direction.x^2 + direction.z^2)))
 
-    -- Create a Vector3 to represent the rotation angles with roll set to 0
-    local rotationAngles = Vector3.new(pitch, yaw, 0)
+    local adjustedRoll = currentRoll -- Placeholder for any roll adjustments
 
-    return rotationAngles
+    return Vector3.new(pitch, yaw, adjustedRoll)
+end
+local function AimAndFireAtTarget(target)
+    local currentpos = instrument:GetReading(3)
+    local newpos = target.Position + (target.AssemblyLinearVelocity * 0.75)
+    -- Aim adjustments for better accuracy
+    newpos = newpos + Vector3.new(0, -0.5, 0) + Vector3.new(-0.5, 0, 0)
+    local rot = CalculateRotation(currentpos, newpos)
+    gyro:LookAtVector(newpos)
+
+    -- Check each raycaster to decide if it's safe to fire
+    local shouldFire = false
+    for _, raycaster in ipairs(raycasters) do
+        local result = raycaster:GetReading()
+        if EvaluateRaycasterTarget(result) then
+            shouldFire = true
+            break
+        end
+    end
+
+    -- Fire if the target is valid and not a friend
+    if shouldFire and target.Health > 0 then
+        switch:Configure({SwitchState = true})
+        -- Additional logic for firing duration or burst firing can be added here
+    else
+        switch:Configure({SwitchState = false})
+    end
 end
 
 function GetTarget(list, name)
@@ -60,133 +109,120 @@ function recursiveprint(t)
     end
 end
 
--- Initialize closest to a non-nil value
-local previous = nil
-while task.wait() do
-    local currentpos = instrument:GetReading(3)
-    local reading = lifesensor:GetReading()
-    local closest = {nil, math.huge, 0}
-    for k, v in pairs(reading) do
-        if isintable(playerwhitelist, v.Name) then
-            continue 
+function StartAiming()
+    aimingCoroutine = coroutine.create(function()
+        local previous = nil
+        while task.wait() do -- make sure we dont murder the server
+            local currentpos = instrument:GetReading(3)
+            local reading = lifesensor:GetReading()
+            local target = FindClosestTarget(reading, currentpos, previous)
+            if target then
+                AimAndFireAtTarget(target)
+                previous = target.ID
+            end
+            coroutine.yield()  -- Yield to allow stopping
         end
-        if v.Health == 0 then continue end
-        -- Set position of closest using magnitude of vector
-        local pos = v.Position
-        local mag = (pos - currentpos).Magnitude
-        -- Adjusted weight calculation
-        local weight = (v.Health / 100 * WEIGHTS.health) + (1 - mag / far * WEIGHTS.distance)
-        if v.ID == previous then weight += WEIGHTS.previous end -- If its the same target, increase weight, since we want to kill it
-        if weight > closest[3] then
-            closest = {v, mag, weight}
+    end)
+    coroutine.resume(aimingCoroutine)
+end
+
+local function parseArgs(argstr:string)
+    return argstr:split(" ")
+end
+
+local function findIndex(t, data)
+    for index, value in t do
+        if value == data then
+            return index
         end
     end
-    -- Check if closest is still non-nil before using it
-    if closest[1] ~= nil then
-        previous = closest[1].ID
-        local newpos = closest[1].Position + (closest[1].AssemblyLinearVelocity * 0.75)
-        -- Aim 2 studs lower now to account for the head and aim slightly right due to model itself (2 studs)
-        newpos = newpos + Vector3.new(0, -0.5, 0) + Vector3.new(-0.5, 0, 0)
-        -- Calculate rotation
-        local rot = CalculateRotation(currentpos, newpos)
-        gyro:LookAtVector(newpos)
-        local count = 0
-        while task.wait() do
-            -- Check even if target is still alive
-            local reading = lifesensor:GetReading()
-            local target = GetTarget(reading, closest[1].ID)
-            -- All passed? point to its new position and shoot
-            local newpos = target.Position + (target.AssemblyLinearVelocity * 0.55)
-            -- Aim 2 studs lower now to account for the head and aim slightly right due to model itself (2 studs)
-            newpos = newpos + Vector3.new(0, -0.5, 0) + Vector3.new(-0.5, 0, 0)
-            gyro:LookAtVector(newpos) -- no need to check if we are hitting a part, most likely sure it barely moves
-            if target ~= nil and target.Health == 0 then
-                break
-            end
-            if not target then break end
-            if count > 60 then break end -- Break after 45 frames
-            local currentrot = instrument:GetReading(4)
-            -- Check magnitude of rotation, if its close enough, break (10 degrees)
-            if (rot - currentrot).Magnitude < 5 then
-                break
-            end
-            count += 1
+    return nil  -- Return nil if data is not found
+end
+
+local methods = {
+    addWhitelist = {
+        reqWhitelist = true,
+        func = function(plr, args)
+            table.insert(playerwhitelist, args[1])
+            ssd:Write("whitelist", JSONEncode(playerwhitelist))
         end
-        local fire = false
-        local result = raycaster:GetReading()
-        if result then
-            pcall(function()
-                -- We dont want to shoot our parts do we?
-                if result.Type == "Humanoid" then fire = true return end
-                -- Check if its the player we want to shoot
-                if result.Type == "Humanoid" and isintable(playerwhitelist, result.Name) then
-                    fire = false
-                    return 
-                end
-                if isintable(playerwhitelist, result.LockedBy) then
-                    fire = false
-                    return
-                elseif result.LockedBy == nil then
-                    fire = true
-                    return
-                elseif result.LockedBy ~= nil then -- Would of catched the first if statement if it were friendly, so it must be an enemy
-                    fire = true
-                    return
-                end
-            end)
-        end
-        if not result then fire = true end
-        -- Check if closest character is still alive before shooting it
-        if fire and closest[1].Health > 0 then
-            switch:Configure({SwitchState = true})
-            local count = 0
-            local fire = false
-            while task.wait() do
-                -- Check raycaster, are we hitting a friendly part?
-                local result = raycaster:GetReading()
-                if result ~= nil then
-                    pcall(function()
-                        -- We dont want to shoot our parts do we?
-                        if result.Type == "Humanoid" and isintable(playerwhitelist, result.Name) then
-                            fire = false
-                            return
-                        end
-                        if result.Type == "Humanoid" and not isintable(playerwhitelist, result.Name) then fire = true return end
-                        -- Check if its the player we want to shoot
-                        if isintable(playerwhitelist, result.LockedBy) then
-                            fire = false
-                            return
-                        elseif result.LockedBy == nil then
-                            fire = true
-                            return
-                        elseif result.LockedBy ~= nil then -- Would of catched the first if statement if it were friendly, so it must be an enemy
-                            fire = true
-                            return
-                        end
-                    end)
-                end
-                if result == nil then fire = true end
-                if fire then switch:Configure({SwitchState = true}) else switch:Configure({SwitchState = false}) end
-                -- Fail if 20 frames have passed
-                if count > 20 then break end
-                -- Read life sensor, check if target is dead yet
-                local reading = lifesensor:GetReading()
-                local target = GetTarget(reading, closest[1].ID)
-                -- Check Health or if nil
-                if not target then break end
-                if target ~= nil and target.Health == 0 then
-                    break
-                end
-                -- All passed? point to its new position and shoot
-                local newpos = target.Position + (target.AssemblyLinearVelocity * 0.60)
-                -- Aim 2 studs lower now to account for the head and aim slightly right due to model itself (2 studs)
-                newpos = newpos + Vector3.new(0, -0.5, 0) + Vector3.new(-0.5, 0, 0)
-                gyro:LookAtVector(newpos) -- no need to check if we are hitting a part, most likely sure it barely moves
-                count += 1
+    },
+    remWhitelist = {
+        reqWhitelist = true,
+        func = function(plr, args)
+            if plr == args[1] then
+                speaker:Chat("You cannot remove yourself!")
+            else
+                table.remove(playerwhitelist, findIndex(playerwhitelist, args[1]))
+                ssd:Write("whitelist", JSONEncode(playerwhitelist))
             end
-            switch:Configure({SwitchState = false})
+        end
+    },
+    start = {
+        reqWhitelist = true,
+        func = function(plr, args)
+            StartAiming()
+        end
+    },
+    stop = {
+        reqWhitelist = true,
+        func = function(plr, args)
+            StopAiming()
+        end
+    },
+    list = {
+        reqWhitelist = true,
+        func = function(plr, args)
+            speaker:Chat("Whitelisted players:")
+            for _, v in pairs(playerwhitelist) do
+                speaker:Chat(v)
+                task.wait(1)
+            end
+        end
+    },
+    init = {
+        reqWhitelist = false,
+        func = function(plr, args)
+            if #playerwhitelist == 0 then
+                table.insert(playerwhitelist, plr.Name)
+                ssd:Write("whitelist", JSONEncode(playerwhitelist))
+                speaker:Chat("You have been added to the whitelist!")
+            else
+                speaker:Chat("The whitelist has already been initialized!")
+            end
+        end
+    },
+}
+
+function StopAiming()
+    if aimingCoroutine then
+        coroutine.close(aimingCoroutine)
+        aimingCoroutine = nil
+    end
+    switch:Configure({SwitchState = false}) -- Make sure the turret is not firing    
+end
+
+function OnChat(plr, msg)
+    if msg:sub(1, prefix:len()) == prefix then
+        local args = parseArgs(msg:sub(prefix:len() + 1))
+        local method = methods[args[1]]
+        -- Remove the command from the args
+        table.remove(args, 1)
+        if method then
+            if method.reqWhitelist and not isintable(playerwhitelist, plr.Name) then
+                speaker:Chat("You are not whitelisted to use this command!")
+            else
+                method.func(plr, args)
+            end
         else
-            switch:Configure({SwitchState = false})
+            speaker:Chat("Invalid command!")
         end
     end
 end
+-- Does the whitelist contain anything?
+if #playerwhitelist == 0 then
+    speaker:Chat("No players are whitelisted! Use !t init to add yourself to the whitelist.")
+end
+
+microphone.Chatted:Connect(OnChat)
+
